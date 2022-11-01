377722cf4008cceafcad1e64e7047c8094177e71
Andrey Viktorov <andrey.viktorov@gmail.com>
2022-11-01 22:28:37 +0700

    Added simple locale sorting stub

--- main.ts
+++ main.ts
@@ -1,137 +1,84 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { FileExplorerView, Plugin, TAbstractFile, TFile, TFolder, Vault } from 'obsidian';
+import { around } from 'monkey-around';
 
-// Remember to rename these classes and interfaces!
+// Needed to support monkey-patching of the folder sort() function
+declare module 'obsidian' {
+	export interface ViewRegistry {
+		viewByType: Record<string, (leaf: WorkspaceLeaf) => unknown>;
+	}
 
-interface MyPluginSettings {
-	mySetting: string;
+	export interface App {
+		viewRegistry: ViewRegistry;
+	}
+
+	interface FileExplorerFolder {
+	}
+
+	export interface FileExplorerView extends View {
+		createFolderDom(folder: TFolder): FileExplorerFolder;
+
+		requestSort(): void;
+	}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+type MonkeyAroundUninstaller = () => void
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+
+export const hexPrefixSort = function(order: string[]) {
+	let fileExplorer = this.fileExplorer;
+	const elements: Array<TAbstractFile> = this.file.children.map((entry: TAbstractFile) => entry);
+
+	elements.sort(function (file0: TAbstractFile, file1: TAbstractFile) {
+		return file0.path.localeCompare(file1.path);
+	});
+
+	const items = elements.map((element: TAbstractFile) => fileExplorer.fileItems[element.path]);
+	this.vChildren.setChildren(items);
+};
+
+export default class HexPrefixSortPlugin extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
+		console.log("loading hex-prefix-sort");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.app.workspace.onLayoutReady(() => {
+			const fileExplorer: FileExplorerView | undefined = this.getFileExplorer();
+			if (!fileExplorer)
+				return;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			if (this.patchFileExplorerFolder(fileExplorer))
+				fileExplorer.requestSort();
+		})
+	}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+	patchFileExplorerFolder(fileExplorer?: FileExplorerView): boolean {
+		fileExplorer = fileExplorer ?? this.getFileExplorer()
+		if (!fileExplorer)
+			return false;
+
+		// @ts-ignore
+		let tmpFolder = new TFolder(Vault, "");
+		let Folder = fileExplorer.createFolderDom(tmpFolder).constructor;
+		const uninstallerOfFolderSortFunctionWrapper: MonkeyAroundUninstaller = around(Folder.prototype, {
+			sort(old: any) {
+				return function (...args: any[]) {
+					return hexPrefixSort.call(this, ...args);
+					// return old.call(this, ...args);
+				};
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		})
+		this.register(uninstallerOfFolderSortFunctionWrapper);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		return true;
+	}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	getFileExplorer(): FileExplorerView | undefined {
+		let fileExplorer: FileExplorerView | undefined = this.app.workspace.getLeavesOfType("file-explorer")?.first()
+			?.view as unknown as FileExplorerView;
+		return fileExplorer;
 	}
 
 	onunload() {
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
